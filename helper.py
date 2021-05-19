6ee8ab19151d25d1282a67b9a641a7fb538e69dd
Kai Berszin <mail@kaibersz.in>
2021-05-19 10:37:04 +0200

    Include datetime filter for fetchLeaderboard

--- helper.py
+++ helper.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+
+def setup_db(conn):
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS votes_history (id INTEGER AUTO_INCREMENT PRIMARY KEY, user_id INTEGER, message_id INTEGER, backer INTEGER, vote_time DATETIME DEFAULT CURRENT_TIMESTAMP)''')
 def getUser(conn, author):
     """
     Query the User
@@ -30,12 +35,18 @@ def removeHistory(conn, message_id, author = None, backer = None):
     conn.commit()
     return c.rowcount > 0
 
-def fetchLeaderboard(conn):
+def fetchLeaderboard(conn, dateFrom = None, dateTo = None):
     """
     Returns the current leaderboard
     """
+    if dateFrom == None:
+        dateFrom = datetime.utcfromtimestamp(0)
+    if dateTo == None:
+        dateTo = datetime.now()
+
+
     c = conn.cursor()
-    c.execute("SELECT user_id, COUNT() as votes FROM votes_history ORDER by votes DESC")
+    c.execute("SELECT user_id, COUNT() as votes FROM votes_history WHERE vote_time >= ? AND vote_time <= ? ORDER by votes DESC", (dateFrom, dateTo))
     return c.fetchall()
 
 def queryHistory(conn, message_id):
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sqlite3 import Error
 import os
 from helper import *
 from discord_helpers import leaderboard, hasRole
+import datetime
 
 DATABASE_NAME = 'votes.db'
 REACTION_TO_COMPARE = '🐻'
@@ -23,10 +24,6 @@ def create_connection(db_file):
 intents = discord.Intents(messages=True, guilds=True, members=True, reactions=True)
 client = discord.Client(intents=intents)
 conn = create_connection(DATABASE_NAME)
-
-def setup_db():
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS votes_history (id INTEGER AUTO_INCREMENT PRIMARY KEY, user_id INTEGER, message_id INTEGER, backer INTEGER, vote_time DATETIME DEFAULT CURRENT_TIMESTAMP)''')
 
 def processVote(message_id, author, backer):
     if updateHistory(conn, author, message_id, backer):
@@ -63,5 +60,5 @@ async def on_reaction_add(reaction, user):
        processVote(reaction.message.id, reaction.message.author.id, user.id)
 
 if __name__ == '__main__':
-    setup_db()
+    setup_db(conn)
     client.run(BOT_TOKEN)
