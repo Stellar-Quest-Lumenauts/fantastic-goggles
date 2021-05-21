4a6dfceb03a09fcca9db7721b297596d8b510eea
Francois Rejete <francois@rejete.com>
2021-05-21 19:23:23 +0900

    adding a handlers module

--- run_schedule.py
+++ run_schedule.py
@@ -1,5 +1,8 @@
 from datetime import datetime
 
+from handlers import distribution
+from handlers import account_missing_warning
+
 if __name__ != "__main__":
     print("run_schedule cannot be imported!")
     exit()
@@ -8,15 +11,9 @@ day = datetime.now().weekday()
 
 if day == 1:  # Monday
     print("Weekly reward distribution!")
-
-    import handle_distribution
-
-    handle_distribution.start()
+    distribution.start()
 elif day == 7:  # Sunday
     print("Reminding lumenauts to set their account!")
-
-    import handle_acc_missing_warning
-
-    handle_acc_missing_warning.start()
+    account_missing_warning.start()
 else:
     print("Nothing to do today!")
