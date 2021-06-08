464cfa49d1c29a87ed2fb3e2a42a39c7c71073a2
François Rejeté <francois@rejete.com>
2021-06-09 02:23:25 +0900

    env variable to enable refractor use

--- helpers/discord.py
+++ helpers/discord.py
@@ -2,10 +2,10 @@ from datetime import datetime
 import discord
 
 from .database import fetchLeaderboard, fetchUserPubKeys
-from .generic import upload_to_hastebin
+from .generic import upload_to_hastebin, post_to_refractor
 from .stellar import fetch_last_tx, fetch_account_balance, generate_reward_tx
 from .graphs import generate_graph
-from settings.default import BASE_FEE
+from settings.default import BASE_FEE, USE_REFRACTOR
 
 
 async def leaderboard(conn, client, message, limit):
@@ -74,15 +74,17 @@ def generate_payouts(conn):
     pricepot = fetch_account_balance() - tx_cost
 
     if len(payoutUser) == 0:
-        return "No eligible users this time!"
+        raise Exception("No eligible users this time!")
     if len(payoutUser) > 100:
-        return "Wow! There are a lot of eligible lumenauts (>100). We should upgrade our code to handle this case..."
+        raise Exception(
+            "Wow! There are a lot of eligible lumenauts (>100). We should upgrade our code to handle this case..."
+        )
     payouts = []
 
     pricepot -= len(payoutUser)  # base reserve * 2 (2 claimaints for each claimableBalance)
 
     if pricepot <= 0:
-        return f"Balance of Pricepot is not high enough to support {len(payoutUser)} eligible lumenauts!"
+        raise Exception(f"Balance of Pricepot is not high enough to support {len(payoutUser)} eligible lumenauts!")
 
     for user in payoutUser:
         payout = user[1] / sumVotes * pricepot
@@ -95,17 +97,20 @@ def generate_report(payouts: list) -> str:
     tx_xdr = generate_reward_tx(payouts, BASE_FEE)
 
     if tx_xdr is None:
-        return "Failed to load reward account!"
+        raise Exception("Failed to load reward account!")
 
     return tx_xdr  # todo size limit?
 
 
 async def notify_submitter(client, conn, user):
     notify_user = await client.fetch_user(user)
-    payouts = generate_payouts(conn)
-    content = generate_report(payouts)
-    if content.startswith("AA"):  # is XDR
-        content = upload_to_hastebin(content)
-    else:
-        content = f"```{content}```"
+    try:
+        payouts = generate_payouts(conn)
+        report = generate_report(payouts)
+        if USE_REFRACTOR:
+            content = post_to_refractor(report)
+        else:
+            content = upload_to_hastebin(report)
+    except Exception as e:
+        content = f"```{e}```"
     await notify_user.send(content=f"New week, new lumenaut rewards:\n{content}")
