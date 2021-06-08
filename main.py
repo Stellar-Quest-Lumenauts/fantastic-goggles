1948c7717ff1d0073711258374d42fedb8dc3241
Matej Plavevski <matej.plavevski@gmail.com>
2021-06-08 15:01:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -57,9 +57,6 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
-
     if message.content.startswith("$$leaderboard"):
         await leaderboard(conn, client, message, LEADERBOARD_LIMIT)
 
@@ -127,7 +124,13 @@ async def _my_pub_key(ctx):
     else:
         await ctx.send("Your account has not been found. Use `/link public_key` to add it to the database.")
 
-
+@slash.slash(
+    name="hello",
+    description="Replies hello",
+)
+async def _hello(ctx):
+    await ctx.send("Hello!")
+        
 if __name__ == "__main__":
     setup_db(conn)
     client.run(DISCORD_BOT_TOKEN)
