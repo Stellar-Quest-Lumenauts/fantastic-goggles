9d946c2843f7f1f71972920169b1688310031e23
Matej Plavevski <matej.plavevski+github@gmail.com>
2022-11-02 23:46:21 +0100

    Linter Errors gone now?

--- main.py
+++ main.py
@@ -34,7 +34,9 @@ if not isinstance(DISCORD_WHITELIST_CHANNELS, list) or (
 
 
 client = Client(token=DISCORD_BOT_TOKEN, intents=Intents.GUILD_MESSAGES | Intents.DIRECT_MESSAGES |
-                Intents.GUILD_MESSAGE_REACTIONS | Intents.GUILD_MESSAGE_CONTENT, presence=ClientPresence(status=StatusType.OFFLINE))
+                Intents.GUILD_MESSAGE_REACTIONS | Intents.GUILD_MESSAGE_CONTENT, 
+                presence=ClientPresence(status=StatusType.OFFLINE)
+                )
 conn = create_connection(DATABASE_NAME)
 
 
